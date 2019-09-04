c2dc057b6dc50a5309af727acdc503979022917e
David Forer <david.j.forer@gmail.com>
2019-09-04 06:37:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,22 +16,27 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 
-	def create
-  @post = Post.new(post_params)
+def create
+  @post = Post.new(post_params(:title, :description))
   @post.save
   redirect_to post_path(@post)
 end
  
 def update
   @post = Post.find(params[:id])
-  @post.update(post_params)
+  @post.update(post_params(:title))
   redirect_to post_path(@post)
 end
  
 private
  
-def post_params
-  params.require(:post).permit(:title, :description)
+ 
+# We pass the permitted fields in as *args;
+# this keeps `post_params` pretty dry while
+# still allowing slightly different behavior
+# depending on the controller action
+def post_params(*args)
+  params.require(:post).permit(*args)
 end
 
 end
